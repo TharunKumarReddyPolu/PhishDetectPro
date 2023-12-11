b6f3c6b3854ffd71267e4d0fdadee97ba2d0a0a1
Polu Tharun Kumar Reddy <polutharun528@gmail.com>
2023-12-10 22:22:00 -0500

    modified model.py

--- app/model/model.py
+++ app/model/model.py
@@ -2,21 +2,36 @@ import pickle
 from pathlib import Path
 import numpy as np
 from app.model.Feature_Extraction import FeatureExtraction
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
 
-__version__ = "0.1.0"
+_version_ = "0.1.0"
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent
 
 class ClassifyURL(FeatureExtraction):
-    def __init__(self):
+    def _init_(self):
         pass
     
     def predict(self, url):
+        print(url)
         with open(f"{BASE_DIR}/phish_classifier.pkl", 'rb') as file:
             pickled_model = pickle.load(file)
-        
-        result = pickled_model.predict(url)
+
+        # Assuming 'url' is the feature and 'label' is the target variable
+        tokenizer = Tokenizer(num_words=5000, oov_token="OOV")
+        tokenizer.fit_on_texts(str(url))
+
+        # Preprocessing the URL
+        random_url_sequence = tokenizer.texts_to_sequences([str(url)])
+        random_url_padded = pad_sequences(random_url_sequence, padding='post', maxlen=100)
+
+        # Reshape for the CNN model
+        random_url_padded_reshaped = np.reshape(random_url_padded, 
+                                                (random_url_padded.shape[0], random_url_padded.shape[1], 1))
+
+        result = pickled_model.predict(random_url_padded_reshaped)
         if result == 0:
             return "Website seems legitimate, Good to Proceed..."
         else:
-            return "Website seems phishing, Beware..."
+            return "Website seems phishing, Beware..."
